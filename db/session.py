9763d4b78157f8eadbc22b197388c6b7deaf61fe
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-12-20 13:04:11 +0200

    added code to the session.py to initialize the database session

---
+++ db/session.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "sqlite:///./football_database.db"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
