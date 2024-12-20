b1cca7641eb19e3338e3bf6aefc8ddf175c5a497
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-12-20 14:15:18 +0200

    added code for the main.py file to control the api

--- api/v1/router.py
+++ api/v1/router.py
@@ -1,1 +1,8 @@
 from fastapi import APIRouter
+from api.v1.endpoints import teams, players, accolades
+
+api_router = APIRouter()
+
+api_router.include_router(teams.router, prefix="/teams", tags=["teams"])
+# api_router.include_router(players.router, tags=["players"])
+# api_router.include_router(accolades.router, tags=["accolades"])
--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 from fastapi import FastAPI
+from uvloop.handles.async_ import async_
 
-app = FastAPI()
+from api.v1.router import api_router
+from db.init_db import init_db
+
+app = FastAPI(title="Football API", description="Manage football teams, players and accolades")
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# include routers
+app.include_router(api_router, prefix="/api/v1")
+
+# initialize database with pre-recorded data
+@app.on_event("startup")
+async def startup_event():
+    init_db()
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World"}
+#
+#
+# @app.get("/hello/{name}")
+# async def say_hello(name: str):
+#     return {"message": f"Hello {name}"}
